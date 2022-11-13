ce2bb91b8a5bdf76b9870b3d9937f03b4e37fb03
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-11-13 23:03:11 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Contacts from './Contacts';
+import { nanoid } from 'nanoid';
+import Section from './Section';
+import Form from './Form';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const onContact = this.state.contacts.some(id => id.name === data.name);
+    if (onContact) {
+      alert(`Contact "${data.name}" is already exist.`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...data, id: nanoid() }],
+    }));
+  };
+
+  onDelete = deleteID => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== deleteID),
+    }));
+  };
+
+  handleInputChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    return (
+      <div className={css.appBody}>
+        <Section title="Phonebook">
+          <Form onSubmit={this.formSubmitHandler} />
+        </Section>
+        <Section title="Contacts">
+          <Contacts
+            contacts={contacts}
+            filter={filter}
+            imputChange={this.handleInputChange}
+            onClickDelete={this.onDelete}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
